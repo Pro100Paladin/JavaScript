6bc00e6597624a93368fe5f316055c9a2845db29
Pro100Paladin <pro100paladin@icloud.com>
2024-09-08 21:33:51 +0300

    hw2

---
+++ homeWork2/task2.js
@@ -0,0 +1,15 @@
+/**
+ * Задание 2
+ * Перепишите код к тернарному оператору
+ *
+ * let test = true;
+ * if (test === true) {
+ * console.log('+++');
+ * } else {
+ * console.log('---');
+ * }
+ */
+
+console.log('Задание 2.');
+let test = true;
+(test === true) ? console.log('+++') : console.log('---');
